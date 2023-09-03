c466f1bf9d765766eac6722f3babc97ef9e11698
anterart <artkirabra@gmail.com>
2023-09-03 21:55:16 +0300

    refactor of the customer service

--- know_your_customer/services/customer.py
+++ know_your_customer/services/customer.py
@@ -12,11 +12,19 @@ def get_dropdown_data(options, answer_value) -> DropdownData:
     return DropdownData(options=options, chosen_option_index=chosen_option_index)
 
 
-def build_out_question(question: Question, answer_value) -> OutQuestion:
+def get_answered_out_question(question: Question, answer_value) -> OutQuestion:
     dropdown_data = None
     if question.question_type == QuestionType.DROPDOWN.value:
         dropdown_data = get_dropdown_data(question.options, answer_value)
     return OutQuestion(**question.model_dump(), answer_value=answer_value, dropdown_data=dropdown_data)
+
+
+def build_out_questions(last_answers, out_questions, question, question_id):
+    if question_id in last_answers:
+        answer = last_answers[question_id]
+        out_questions[question_id] = get_answered_out_question(question, answer.value)
+    else:
+        out_questions[question_id] = OutQuestion(**question.model_dump())
 
 
 def get_questionnaire_obj(customer_id: int) -> Questionnaire:
@@ -29,11 +37,7 @@ def get_questionnaire_obj(customer_id: int) -> Questionnaire:
     questions = get_questions()
 
     for question_id, question in questions.items():
-        if question_id in last_answers:
-            answer = last_answers[question_id]
-            out_questions[question_id] = build_out_question(question, answer.value)
-        else:
-            out_questions[question_id] = OutQuestion(**question.model_dump())
+        build_out_questions(last_answers, out_questions, question, question_id)
 
     questionnaire = Questionnaire(customer=customer, questions=out_questions)
     return questionnaire
