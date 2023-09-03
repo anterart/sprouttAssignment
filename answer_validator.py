aa0a07f461e1d92ac9d096afc8f5f7e2536dad45
anterart <artkirabra@gmail.com>
2023-09-03 22:05:14 +0300

    Create an answer_validator.py module

---
+++ answer_validator.py
@@ -0,0 +1,40 @@
+from enums import QuestionType, YesNoQuestion
+from exceptions import ItemNotFoundError, ValueDoesntFitToQuestionTypeError
+
+
+def validate_answer(answer, questions):
+    if answer.question_id not in questions:
+        raise ItemNotFoundError(f'There is no question for question_id {answer.question_id} in db for answer {answer}')
+
+    question = questions[answer.question_id]
+    if question.question_type == QuestionType.NUMBER.value:
+        validate_numeric_question_answer(answer)
+
+    elif question.question_type == QuestionType.YES_NO.value:
+        validate_yes_no_question_answer(answer)
+
+    elif question.question_type == QuestionType.DROPDOWN.value:
+        validate_answer_exists_in_options(answer, question)
+
+
+def validate_answer_exists_in_options(answer, question):
+    if answer.value is None:
+        return
+
+    if answer.value not in question.options:
+        raise ValueDoesntFitToQuestionTypeError(f'{answer.value} cant be chosen for this question_id'
+                                                f' {answer.question_id}')
+
+
+def validate_yes_no_question_answer(answer):
+    if answer.value not in {opt.value for opt in YesNoQuestion}:
+        raise ValueDoesntFitToQuestionTypeError(f'{answer.value} is not of type {QuestionType.YES_NO.value} for'
+                                                f' question_id {answer.question_id}')
+
+
+def validate_numeric_question_answer(answer):
+    try:
+        float(answer.value)
+    except ValueError:
+        raise ValueDoesntFitToQuestionTypeError(f'{answer.value} is not of type {QuestionType.NUMBER.value} for'
+                                                f' question_id {answer.question_id}')
--- know_your_customer/services/answers.py
+++ know_your_customer/services/answers.py
@@ -1,8 +1,8 @@
 from typing import List
 from models import Answer, HistoricalAnswer
-from exceptions import ItemNotFoundError, ValueDoesntFitToQuestionTypeError
-from enums import QuestionType, YesNoQuestion
+from exceptions import ItemNotFoundError
 from db.data_access import get_questions, insert_answer, update_customer_last_answers, get_customer
+from answer_validator import validate_answer
 
 
 def insert_customer_answers(customer_id: int, answers: List[Answer]):
@@ -22,41 +22,3 @@ def insert_customer_answers(customer_id: int, answers: List[Answer]):
         insert_answer(historical_answer)
 
     update_customer_last_answers(customer_id, answers)
-
-
-def validate_answer(answer, questions):
-    if answer.question_id not in questions:
-        raise ItemNotFoundError(f'There is no question for question_id {answer.question_id} in db for answer {answer}')
-
-    question = questions[answer.question_id]
-    if question.question_type == QuestionType.NUMBER.value:
-        validate_numeric_question_answer(answer)
-
-    elif question.question_type == QuestionType.YES_NO.value:
-        validate_yes_no_question_answer(answer)
-
-    elif question.question_type == QuestionType.DROPDOWN.value:
-        validate_answer_exists_in_options(answer, question)
-
-
-def validate_answer_exists_in_options(answer, question):
-    if answer.value is None:
-        return
-
-    if answer.value not in question.options:
-        raise ValueDoesntFitToQuestionTypeError(f'{answer.value} cant be chosen for this question_id'
-                                                f' {answer.question_id}')
-
-
-def validate_yes_no_question_answer(answer):
-    if answer.value not in {opt.value for opt in YesNoQuestion}:
-        raise ValueDoesntFitToQuestionTypeError(f'{answer.value} is not of type {QuestionType.YES_NO.value} for'
-                                                f' question_id {answer.question_id}')
-
-
-def validate_numeric_question_answer(answer):
-    try:
-        float(answer.value)
-    except ValueError:
-        raise ValueDoesntFitToQuestionTypeError(f'{answer.value} is not of type {QuestionType.NUMBER.value} for'
-                                                f' question_id {answer.question_id}')
